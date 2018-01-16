f2ecf44cf533d61bc77bf928f7342844da85d16d
admin <isalechat@yahoo.com>
2018-01-16 17:38:21 +0100

    Simon says, in the middle

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -3,10 +3,23 @@
 def add(param1,param2)
   add=param1+param2
 end
-
 def subtract(param1,param2)
   substract=param1-param2
 end
 
-def sum
+def sum(array)
+  if array.empty?
+    sum=0
+  else
+    for i in 0..(array.length-1)
+      sum = sum + array[i]
+    end
+  end
+  # unless array.empty?
+  #   for i in 0..array.length-1
+  #     sum += array[i]
+  #   end
+  # else
+  #   sum=0
+  # end
 end
--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,13 @@
-#write your code here
+def echo(hello)
+  "#{hello}"
+end
+def shout(shout)
+  "#{shout}".upcase
+end
+def repeat(string,nb=2)
+  result = "#{string} "*nb
+  result.rstrip
+end
+def start_of_word(string,nb=1)
+    string[nb-1]
+end
