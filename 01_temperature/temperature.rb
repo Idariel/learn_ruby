fff82e0fbac740a59b34895f3e31fba6233f1de1
admin <isalechat@yahoo.com>
2018-01-16 12:39:32 +0100

    petites corrections pour du code propre

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,7 +1,6 @@
 def ftoc(degre_F)
   degre_C = (degre_F - 32)*(5.0/9)
 end
-
 def ctof(degre_C)
   degre_F = degre_C*(9.0/5) + 32
 end
--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,12 @@
-#write your code here
+# `sum` takes an *array* of parameters and adds them all together
+
+def add(param1,param2)
+  add=param1+param2
+end
+
+def subtract(param1,param2)
+  substract=param1-param2
+end
+
+def sum
+end
